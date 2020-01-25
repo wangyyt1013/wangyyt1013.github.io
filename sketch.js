cf2ecb56bccd19233f57466964cd2666c480b0a9
wangyyt1013 <yw3241@columbia.edu>
2020-01-24 22:46:34 -0500

    updated sketch

--- sketch.js
+++ sketch.js
@@ -1,38 +1,65 @@
-var x = new Array(20);  // **change** float[] x = new float[20] to new Array(20)
-var y = new Array(20);  // **change** float[] y = new float[20] to new Array(20)
-var segLength = 18;                                 // **change** float to var
+const Y_AXIS = 1;
+var i, j;
 
-function setup() {                          // **change** void setup() to function setup()
-  createCanvas(640, 360);                   // **change** size() to createCanvas()
-  strokeWeight(9);                          // strokeWeight() is the same
-  stroke(255, 100);                         // stroke() is the same
-  for(var i=0; i<x.length; i++) {         // initialize the array
-    x[i]=0;
-    y[i]=0;
+function setup() {
+  createCanvas(800, 800);
+}
+
+function draw() {
+  background(0);
+  var c1 = color(132, 225, 255);
+  var c2 = color(0, 0, 0)
+  var curHeight = (1 - hour()/24) * height;
+
+  setGradient(0, 0, width, curHeight, c1, c2, Y_AXIS);
+  noStroke();
+  fill(255, 255, 132);
+  for (i = curHeight; i > 0; i = i - 40){
+    for (j = Math.random() * 20; j < width; j += 40){
+      circle(j, i, 5);
+    }
   }
-}
 
-function draw() {                           // **change** void draw() to function draw()
-  background(0);                            // background() is the same
-  drawSegment(0, mouseX, mouseY);           // functions calls, mouseX and mouseY are the same
-  for(var i=0; i<x.length-1; i++) {         // **change** int i to var i
-    drawSegment(i+1, x[i], y[i]);           // function calls are the same
+  translate(400, 400);
+
+  var shade = 10 + hour() * 10;
+  fill(shade, shade, 0);
+  ellipse(0, 0, 50, 50);
+
+  strokeWeight(1);
+  for (i = 0; i < minute(); i++){
+    let rad = 60 + i/60 * 700;
+    noFill();
+    rotate(PI*second()/60);
+
+    stroke(shade, shade, 0);
+    ellipse(0, 0, rad, rad);
+
+    fill(255);
+    stroke(0);
+    circle(0, rad/2, 5);
   }
+
 }
 
-function drawSegment(i, xin, yin) {         // **change** void drawSegment() to function drawSegment(), remove type declarations
-  var dx = xin - x[i];                      // **change** float to var
-  var dy = yin - y[i];                      // **change** float to var
-  var angle = atan2(dy, dx);                // **change** float to var, atan2() is the same
-  x[i] = xin - cos(angle) * segLength;      // cos() is the same
-  y[i] = yin - sin(angle) * segLength;      // sin() is the same
-  segment(x[i], y[i], angle);               // function calls are the same
-}
+function setGradient(x, y, w, h, c1, c2, axis) {
+  noFill();
 
-function segment(x, y, a) {                 // **change** void segment() to function segment(), remove type declarations
-  push();                            		// pushMatrix() becomes push()
-  translate(x, y);                          // translate() is the same
-  rotate(a);                                // rotate() is the same
-  line(0, 0, segLength, 0);                 // line() is the same
-  pop();                              		// popMatrix() becomes pop()
+  if (axis === Y_AXIS) {
+    // Top to bottom gradient
+    for (let i = y; i <= y + h; i++) {
+      let inter = map(i, y, y + h, 0, 1);
+      let c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(x, i, x + w, i);
+    }
+  } else if (axis === X_AXIS) {
+    // Left to right gradient
+    for (let i = x; i <= x + w; i++) {
+      let inter = map(i, x, x + w, 0, 1);
+      let c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(i, y, i, y + h);
+    }
+  }
 }
